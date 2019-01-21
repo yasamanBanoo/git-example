72e973b3e83e259d779af7b22e7d5a470ee37e5e
Yasaman Raiszadeh <y_raiszadeh@cid.ir>
2019-01-21 11:40:13 +0330

    const port is added

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello');
 });
 
-app.listen(3000, () => {
-    console.log('server is running');
+app.listen(port, () => {
+    console.log(`server is running onn port ${port}`);
 });
